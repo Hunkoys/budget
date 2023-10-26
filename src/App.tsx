c375b3ebe00db7353d6bd860b672c3c01881f58f
doedash <146763271+doedash@users.noreply.github.com>
2023-10-26 16:33:48 +0000

    bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.scss'
 
-const items = {
+const creditItems = {
   Car: 1301,
   Personal1: 300
 }
 
+const wantItems = {
+  Macbook: 2500,
+  Xreal: 400
+}
+
 function App() {
 
-  const [credits, setCredits]: [Items, Function] = useState(items)
+  const [credits, setCredits]: [Items, Function] = useState(creditItems)
+  const [wants, setWants]: [Items, Function] = useState(wantItems)
+  const [earned, setEarned] = useState(0)
 
-  const total = sum(credits)
+  const onChange = useCallback((e) => setEarned(e.target.value), [])
+
+  const total = sum(credits) + sum(wants)
 
   return (
-    <div className='bar'>
-      <div className="progress"></div>
+    <div className='chart'>
+      <div className="progress">
+        <div className="bar" style={{ height: percent(earned, total) }}></div>
+      </div>
       <div className="milestones">{
-        Object.entries(credits).map(([name, c]: [string, number]) => {
+        Object.entries({ ...credits, ...wants }).reverse().map(([name, c]: [string, number]) => {
           return <div className='stone' style={{ height: percent(c, total) }}>{c + " " + name}</div>
         })
       }
       </div>
+      <input type="text" onChange={onChange} value={earned} />
     </div>
   )
 }
