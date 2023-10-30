b46edb7596533567bd7846a4529cfdf977ef0058
doedash <146763271+doedash@users.noreply.github.com>
2023-10-30 21:16:34 +0000

    user input value

--- src/App.tsx
+++ src/App.tsx
@@ -11,28 +11,48 @@ const wantItems = {
   Xreal: 400
 }
 
+
 function App() {
 
-  const [credits, setCredits]: [Items, Function] = useState(creditItems)
-  const [wants, setWants]: [Items, Function] = useState(wantItems)
+  const [credits, setCredits]: [Items, Function] = useState({})
+  const [wants, setWants]: [Items, Function] = useState({})
   const [earned, setEarned] = useState(0)
+
+  const [creditsText, setCreditsText] = useState()
+  const [wantsText, setWantsText] = useState()
+  const creditsOnChange = useCallback((e: any) => {
+    setCreditsText(e.target.value)
+    setCredits(parseData(e.target.value))
+  }, [])
+  const wantsOnChange = useCallback((e: any) => {
+    setWantsText(e.target.value)
+    setWants(parseData(e.target.value))
+  }, [])
 
   const onChange = useCallback((e: any) => setEarned(e.target.value), [])
 
   const total = sum(credits) + sum(wants)
+  console.log(total);
+
 
   return (
-    <div className='chart'>
-      <div className="progress">
-        <div className="bar" style={{ height: percent(earned, total) }}></div>
+    <div className="app">
+      <div className='chart'>
+        <div className="progress">
+          <div className="bar" style={{ height: percent(earned, total) }}></div>
+        </div>
+        <div className="milestones">{
+          Object.entries({ ...credits, ...wants }).reverse().map(([name, c]: [string, number]) => {
+            return <div className='stone' style={{ height: percent(c, total) }}>{c + " " + name}</div>
+          })
+        }
+        </div>
+        <input type="text" onChange={onChange} value={earned} />
       </div>
-      <div className="milestones">{
-        Object.entries({ ...credits, ...wants }).reverse().map(([name, c]: [string, number]) => {
-          return <div className='stone' style={{ height: percent(c, total) }}>{c + " " + name}</div>
-        })
-      }
+      <div className="data">
+        <textarea className="credits" value={creditsText} onChange={creditsOnChange} ></textarea>
+        <textarea className="wants" value={wantsText} onChange={wantsOnChange} ></textarea>
       </div>
-      <input type="text" onChange={onChange} value={earned} />
     </div>
   )
 }
@@ -43,10 +63,26 @@ interface Items {
   [index: string]: number;
 }
 
+function parseData(data: string): Items {
+  const list: any = data.split('\n').map(item => item.split(' '));
+  const items: Items = {}
+  list.forEach((([key, value]: [string, number]) => {
+    items[key] = Number(value)
+  }))
+  console.log(items);
+
+  return items
+}
+
+
 function sum(list: Items): number {
-  return Object.values(list).reduce((p: number, c: number) => {
-    return p + c
-  })
+  const array = Object.values(list)
+  console.log(array.length)
+  if (array.length)
+    return array.reduce((p: number, c: number) => {
+      return p + c
+    })
+  else return Number(0)
 }
 
 function percent(n: number, cent: number): string {
